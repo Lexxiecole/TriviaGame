3d68f1cfec6ce3e0604abe406b2d05b94f8d77ee
Alexandra <Lexxiecole@gmail.com>
2018-08-28 15:35:48 -0400

    psuedo coding

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+//question 1//
+
+//when page loads, timer starts
+//if user clicks option 2, a giph shows up of a happy plant.
+//else, giph of dying plant pops up
+
+//user must click answer before the timer is up
+ //once first question is answered, timer starts for second question.
+//show timer????
+
+//question 2//
+
+//question 3//
+
+//question 4//
