71b522a30a96241b4d0acb1f959d220802348d88
Alexandra <Lexxiecole@gmail.com>
2018-08-28 20:26:23 -0400

    stop doubting yourselfgit add .git add .git add .

--- assets/javascript/practice.js
+++ assets/javascript/practice.js
@@ -1,13 +1,11 @@
 
 
 //my idea
-var secondsleft = 60
+var secondsleft = 15
+//go reset and go// //google this
 
 //this is from w3schools, being changed.
 //15 seconds to answer each question
-var seconds = Math.floor((distance % (1000 * 15)) / 1000);
-document.getElementById("demo").innerHTML = seconds + " seconds left";
-if (distance < 0) {
     //then display a losing gif,
     //AND tell user to go to the next question
 }
