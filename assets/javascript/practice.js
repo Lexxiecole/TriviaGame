ecd0c88d6d32cbc536bf054f4ed956226146863a
Alexandra <Lexxiecole@gmail.com>
2018-08-28 20:06:41 -0400

    more psuedo coding, i feel lie this is getting super complicated

--- assets/javascript/practice.js
+++ assets/javascript/practice.js
@@ -11,3 +11,7 @@ if (distance < 0) {
     //then display a losing gif,
     //AND tell user to go to the next question
 }
+//if user selects correct answer before timer is up, gif shows that they got the correct answer for 5 seconds,
+//then the gif goes away, and the timer starts for the next question
+//if the user selects the incorrect answer before the timer is up, timer pauses, and incorrect gif is shown for 5 seconds
+//then the timer resumes until time runs out
